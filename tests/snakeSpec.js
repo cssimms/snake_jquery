d651cbe06630ecd6b6c826b02fd524c388cdc83e
Megan Anderson <megan@Megans-MacBook-Pro.local>
2017-10-12 14:25:56 -0700

    karmelized

--- tests/snakeSpec.js
+++ tests/snakeSpec.js
@@ -1,8 +1,7 @@
 describe('Snake Object', () => {
-
+    console.log('*******snakey********')
     it('instantiates with a head', () => {
         let snake = new Snake();
-
         expect(snake.head).not.toBeNull();
     });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,31 @@
+var webpack = require('webpack');
+
 module.exports = {
   context: __dirname,
   entry: "./js/main.js",
   output: {
-    path: "./js",
-    publicPath: "/js/",
+    path: __dirname,
+
     filename: "bundle.js",
     devtoolModuleFilenameTemplate: '[resourcePath]',
     devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
   },
-  devtool: 'source-maps'
+  devtool: 'source-maps',
+  plugins: [
+    new webpack.LoaderOptionsPlugin({
+      // test: /\.xxx$/, // may apply this only for some modules
+      options: {
+        rules: [
+          {
+            test: /\.js$/,
+            exclude:/(node_modules)/,
+            loader:"babel-loader",
+            query:{
+              presets:["env"]
+            }
+          }
+        ]
+      }
+    })
+  ]
 };
